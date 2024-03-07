4f955581c23113e2bead9f1f963396d9f5f4a5ee
Zahid Hasan <jahidhasann67@gmail.com>
2024-03-07 13:02:14 +0600

    organize code and handle exceptions

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import time
 from selenium import webdriver
-
 from pages.men_page import MenPage
 from pages.all_men_page import AllMenPage
 
+PRODUCTS_TO_SCRAPE = 200
 
-def start_scrapping():
+def start_scraping():
     start_time = time.time()
 
-    driver = webdriver.Firefox()
+    with webdriver.Firefox() as driver:
+        men_page = MenPage(driver)
+        men_page.go_to_men_page()
+        men_page.go_to_all_men_page()
 
-    men_page = MenPage(driver)
-    men_page.go_to_men_page()
-    men_page.go_to_all_men_page()
+        all_men_page = AllMenPage(driver)
+        all_men_page.get_products(PRODUCTS_TO_SCRAPE)
 
-    all_men_page = AllMenPage(driver)
-    all_men_page.get_products(200)
-    driver.quit()
-
-    print("Code runtime:", time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
-
+    elapsed_time = time.time() - start_time
+    print(f"Code runtime: {time.strftime('%H:%M:%S', time.gmtime(elapsed_time))}")
 
 if __name__ == '__main__':
-    start_scrapping()
+    start_scraping()
--- pages/men_page.py
+++ pages/men_page.py
@@ -3,17 +3,20 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class MenPage:
-    def __init__(self, driver):
-        self.driver = driver
-
     MEN_PAGE_URL: str = "https://shop.adidas.jp/men"
     ALL_MEN_TEXT: tuple[str, str] = (By.XPATH, "//a[@data-ga-event-label='mens-all']")
+
+    def __init__(self, driver):
+        self.driver = driver
 
     def go_to_men_page(self):
         self.driver.get(self.MEN_PAGE_URL)
 
     def go_to_all_men_page(self):
-        all_men_element = WebDriverWait(self.driver, 60).until(
-            EC.element_to_be_clickable(self.ALL_MEN_TEXT)
-        )
-        all_men_element.click()
+        try:
+            all_men_element = WebDriverWait(self.driver, 60).until(
+                EC.element_to_be_clickable(self.ALL_MEN_TEXT)
+            )
+            all_men_element.click()
+        except Exception as e:
+            print(f"Error while navigating to all men's page: {e}")
